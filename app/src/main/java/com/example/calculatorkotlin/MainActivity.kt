f07a09fae8ea012ade1fb4ac9aef46af95ef1a42
EDC <manujanethwidu@gmail.com>
2020-02-11 14:57:20 +0530

    Start2

--- app/src/main/java/com/example/calculatorkotlin/MainActivity.kt
+++ app/src/main/java/com/example/calculatorkotlin/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.calculatorkotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +19,11 @@ class MainActivity : AppCompatActivity() {
     fun buNumberEvent(view: View) {
         val EntryData = etShowNumber.text
         val buSelect = view as Button
+
+        if (isNewOp) {
+            etShowNumber.setText("")
+        }
+        isNewOp = false
         var buClickValue: String = etShowNumber.text.toString()
         when (buSelect.id) {
             bu1.id -> {
@@ -55,5 +62,57 @@ class MainActivity : AppCompatActivity() {
             }
         }
         etShowNumber.setText(buClickValue)
+    }
+
+    var op = "*"
+    var oldNumber = ""
+    var isNewOp = true
+    fun buOppEvent(view: View) {
+
+        val buSelect = view as Button
+        when (buSelect.id) {
+            buMul.id -> {
+                op = "*"
+            }
+            buDev.id -> {
+                op = "/"
+            }
+            buMinus.id -> {
+                op = "-"
+            }
+            buAdd.id -> {
+                op = "+"
+            }
+        }
+        oldNumber = etShowNumber.text.toString()
+        isNewOp = true
+
+        val toast = Toast.makeText(applicationContext, op, Toast.LENGTH_SHORT)
+        toast.show()
+
+
+    }
+
+    fun buEqual(view: View) {
+        val newNumber = etShowNumber.text.toString()
+        var finalNo: Double? = null
+
+        when (op) {
+            "*" -> {
+                finalNo = oldNumber.toDouble() * newNumber.toDouble()
+            }
+            "/" -> {
+                finalNo = oldNumber.toDouble() / newNumber.toDouble()
+            }
+            "-" -> {
+                finalNo = oldNumber.toDouble() - newNumber.toDouble()
+            }
+            "+" -> {
+                finalNo = oldNumber.toDouble() + newNumber.toDouble()
+            }
+        }
+        etShowNumber.setText(finalNo.toString())
+        isNewOp = true
+
     }
 }
